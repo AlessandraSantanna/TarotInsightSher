d7a4e5154ec574a3838c8578ea83d86b42217cf3
AlessandraSantanna <alesy2007@gmail.com>
2024-10-17 11:26:34 -0300

    sherTarot

---
+++ cartas.js
@@ -0,0 +1,40 @@
+function showMessage() {
+    document.getElementById("message").innerHTML = "<span style='color:red;'>O Diabo </span> <br>Precisamos tomar cuidado com o quanto desejamos algo para não cairmos em armadilhas do ego e para não nos deixarmos cair em obsessão por algo. Reflita sobre o motivo e o quanto você deseja o objeto dos seus sonhos (seja esse sonho um relacionamento, um trabalho, um bem material), ok? No amor, cuidado com ciúmes e brigas desnecessárias. No profissional, você pode entrar em uma competitividade enorme em relação a outros profissionais.";
+}
+
+function showMessage2() {
+    document.getElementById("message").innerHTML = "<span style='color:red;'>O Julgamento </span> <br> faz com que planos de hoje saiam bem diferentes do planejado! Essa carta geralmente traz mudanças significativas naquilo que achávamos que podíamos controlar. O conselho é um só: se abra e dance conforme a música! No amor, você pode receber mensagens e informações que não estava esperando - isso pode ter grande impacto no que você pode esperar daqui pra frente. No profissional, propostas e funções inesperadas também podem surgir. O gostoso dessa carta é nos lembrar de que tudo pode ser surpreendente, que imprevistos não são o fim do mundo e que há sempre um jeito novo de ver a  ida!";
+}
+function showMessage3() {
+    document.getElementById("message").innerHTML = "<span style='color:red;'>A Lua </span> <br> Traz um contato mais intimo com suas emoções e intuição, quanto um momento de incertezas e dúvidas. Com este arcano_ tenha cuidado para não se deixar levar por paranóias e para não se perder dentro das próprias deduções, ok? No amor, você pode acabar nutrindo alguma Ilusão. Seja mais realista. As coisas não estão tão profundas quanto você está vendo.";
+}
+function showMessage4() {
+    document.getElementById("message").innerHTML = "<span style='color:red;'>O mundo  </span> <br> Nos sentiremos alcançando um ciclo completo em nossas vidas, celebrando conquistas e nos preparando para novos começos. Uma etapa se fecha para outra se abrir - e isso soará perfeitamente natural para você. No amor, é favorável para encontros românticos.  No lado profissional, projetos estão sendo concluídos e novos convites podem ser esperados. Abra os olhos para oportunidades além da sua bolha, ok?";
+}
+function showMessage5() {
+    document.getElementById("message").innerHTML = "<span style='color:red;'>Os Enamorados </span> <br>Espere por um dia passional e repleto de dúvidas ao mesmo tempo, no qual você sentirá que precisa fazer uma escolha importante. Siga o seu coração e a sua No amor, é favorável para encontros e trocas afetuosas. No trabalho, parcerias podem se fortalecer e você se sentirá mais criativa e artista para cumprir suas funções. Apesar das dúvidas, essa carta traz a visão de um mundo  mais otimista.";
+}
+
+
+function showMessage6() {
+    document.getElementById("message").innerHTML = "<span style='color:red;'>O Sol </span> <br> Este arcano traz otimismo, realização,esclarecimento de dúvidas e muita visibilidade. O Sol não só ilumina os caminhos com confiança e fartura, como também sugere que hoje é o dia de se expor de forma saudável. Fale o que deseja, mostre-se ao mundo e aproveite essa energia radiante.!";
+}
+function showMessage7() {
+    document.getElementById("message").innerHTML = "<span style='color:red;'>O Mago </span> <br> representa um dia de lampejos criativos e força de manifestação. Ele marca o surgimento de um novo foco em sua vida. Agora, será necessário manter esse foco e Planejar como tudo será executado. É provável que você faça algo completamente novo hoje, seja no trabalho, na vida pessoal, nos romances ou na espiritualidade. O Mago é um princípio criativo!";
+}
+function showMessage8() {
+    document.getElementById("message").innerHTML = "<span style='color:red;'>A Força  </span> <br> o dia pede Foco, disciplina e a capacidade de superar impulsos,desejos e vontades que podem nos sabotar ou nos levar ao auto-boicote. A Força nos mostra que o foco é a nossa melhor escolha quando temos objetivos a atingir.Com essa carta, vamos identificar o que nos tira do nosso eixo e exercer um domínio consciente sobre isto! Aquela carta que significa fazer realmente Força mas por fora estar plena!";
+}
+function showMessage9() {
+    document.getElementById("message").innerHTML = "<span style='color:red;'>O Diabo </span> <br>Precisamos tomar cuidado com o quanto desejamos algo para não cairmos em armadilhas do ego e para não nos deixarmos cair em obsessão por algo. Reflita sobre o motivo e o quanto você deseja o objeto dos seus sonhos (seja esse sonho um relacionamento, um trabalho, um bem material), ok? No amor, cuidado com ciúmes e brigas desnecessárias. No profissional, você pode entrar em uma competitividade enorme em relação a outros profissionais.";
+}
+
+function showMessage10() {
+    document.getElementById("message").innerHTML = "<span style='color:red;'>O Julgamento </span> <br> faz com que planos de hoje saiam bem diferentes do planejado! Essa carta geralmente traz mudanças significativas naquilo que achávamos que podíamos controlar. O conselho é um só: se abra e dance conforme a música! No amor, você pode receber mensagens e informações que não estava esperando - isso pode ter grande impacto no que você pode esperar daqui pra frente. No profissional, propostas e funções inesperadas também podem surgir. O gostoso dessa carta é nos lembrar de que tudo pode ser surpreendente, que imprevistos não são o fim do mundo e que há sempre um jeito novo de ver a  ida!";
+}
+function showMessage11() {
+    document.getElementById("message").innerHTML = "<span style='color:red;'>A Lua </span> <br> Traz um contato mais intimo com suas emoções e intuição, quanto um momento de incertezas e dúvidas. Com este arcano_ tenha cuidado para não se deixar levar por paranóias e para não se perder dentro das próprias deduções, ok? No amor, você pode acabar nutrindo alguma Ilusão. Seja mais realista. As coisas não estão tão profundas quanto você está vendo.";
+}
+function showMessage12() {
+    document.getElementById("message").innerHTML = "<span style='color:red;'>O mundo  </span> <br> Nos sentiremos alcançando um ciclo completo em nossas vidas, celebrando conquistas e nos preparando para novos começos. Uma etapa se fecha para outra se abrir - e isso soará perfeitamente natural para você. No amor, é favorável para encontros românticos.  No lado profissional, projetos estão sendo concluídos e novos convites podem ser esperados. Abra os olhos para oportunidades além da sua bolha, ok?";
+}
